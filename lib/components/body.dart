a0ff3daf4132ed22caaa110634410b49df69220f
CreatorValentina <valentina_mauriello@outlook.it>
2021-09-19 11:47:25 +0200

    Secondo commit

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -16,8 +16,44 @@ class Body extends  StatelessWidget {
         children: [
           HeaderWithSearchBox(size: size),
           TitleWithMoreButton( title: 'Consigliati',),
-          Container(
+                Container(
+            margin: EdgeInsets.only(
+              left: kDefaultPadding,
+              top: kDefaultPadding/2,
+              bottom: kDefaultPadding * 2.5,
+            ),
             width: size.width*40,
+            child: Column(
+              children: [
+                Image.asset('assets/images/image_1.png'),
+                Container(
+                  padding : EdgeInsets.all(kDefaultPadding/2),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0,10),
+                        blurRadius: 50,
+                        color: kPrimaryColor.withOpacity(0.23),
+                      )
+                    ]
+                  ),
+                  child: Row(
+                    children: [
+                      RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                              text: "Samantha".toUpperCase(),
+                              style: Theme.of(context).textTheme.button,)
+                              ],
+                        )
+                      )
+                    ],
+                  )
+                )
+              ],
+            )
           )
         ],
       ),
